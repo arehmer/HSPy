4c7db3a68cac3298966662bc85310be99b4828c6
Rehmer <alexander.rehmer@gmail.com>
2026-05-12 15:20:07 +0200

    printouts removed

--- hspy/ipc/threads.py
+++ hspy/ipc/threads.py
@@ -310,8 +310,7 @@ class DataRecord(RThread_R1):
                          **kwargs)
         
         # Assign user specified attributes
-        self.save_dir = kwargs.pop('save_dir',Path.cwd())                        # Directory to write results and recorded data to
-        print(f'2: {self.save_dir}')
+        self.save_dir = save_dir                                                # Directory to write results and recorded data to
         self.counter = 0                                                        # File counter, incremented after every write operation
         
         
@@ -343,9 +342,6 @@ class DataRecord(RThread_R1):
         # rec_dir is named after the date and time of its creation
         formatted_datetime = datetime.now().strftime("%d_%m_%y_%H%M%S")
         self.rec_dir = self.save_dir / formatted_datetime
-        
-        print(f'3: {self.save_dir}')
-        print(f'4: {self.rec_dir}')
         
         # Create rec_dir and all its parents if necessary
         self.rec_dir.mkdir(parents=True,
