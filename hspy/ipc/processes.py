35ef3af9b8b08ec283047d09a4034421fbcc8541
Rehmer <alexander.rehmer@gmail.com>
2026-08-21 13:45:19 +0200

    Update processes.py
    delted printout

--- hspy/ipc/processes.py
+++ hspy/ipc/processes.py
@@ -372,7 +372,6 @@ class CVProcess(RWProcess_R1):
                 frame = frame.reshape(self.tparray._npsize)
 
             try:
-                print(frame)
                 data_processed = self.model.process({'frame': frame,
                                                      'idx': data['image_id']})
                 data.update(data_processed)
