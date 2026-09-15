4f4b0cd331442464d055b14827d3b5e9b2b6e28e
Rehmer <alexander.rehmer@gmail.com>
2026-09-15 09:35:57 +0200

    Update setup.py
    Added *.json files as package_data

--- setup.py
+++ setup.py
@@ -5,10 +5,16 @@ with open("config/requirements.txt") as requirement_file:
 
 setup(
     name="HSPy",
-    description="A simple package.",
-    version="1.0.0",
-    author="Name",
-    author_email="Name@domain.com",
+    description="Python Interface for Heimann Sensor Thermopile Arrays.",
+    version="0.0.1",
+    author="Alexander Rehmer",
+    author_email="rehmer@heimannsensor.com",
     install_requires=requirements,
     packages=find_packages(), # package = any folder with an __init__.py file
+    package_data={
+        # "" means apply to all packages; adjust the key to a specific
+        # package name if you only want it in one folder, e.g. "HSPy.data"
+        "hspy.arraytypes": ["*.json"],
+    },
+    include_package_data=True
 )
